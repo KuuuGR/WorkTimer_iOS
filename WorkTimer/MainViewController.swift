a50afd6729f6d9b70b6e8a5fe131d6de17a39fe1
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-04-09 12:14:23 +0200

    add number of pressed button

--- WorkTimer/MainViewController.swift
+++ WorkTimer/MainViewController.swift
@@ -14,29 +14,35 @@ class MainViewController: UIViewController {
     @IBOutlet weak var jobButton: UIButton!
     @IBOutlet weak var eatButton: UIButton!
     
+    var currentSelectButton: Int = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(currentSelectButton)
         // Do any additional setup after loading the view.
     }
 
     
     @IBAction func drinkButtonPressed(_ sender: Any) {
+        currentSelectButton = 1
         onlyOneButtonMarked(drinkButton)
     }
     
     @IBAction func jobButtonPressed(_ sender: Any) {
+        currentSelectButton = 2
         onlyOneButtonMarked(jobButton)
     }
     
     @IBAction func eatButtonPressed(_ sender: Any) {
+        currentSelectButton = 3
         onlyOneButtonMarked(eatButton)
     }
     
     
     func onlyOneButtonMarked(_ Button: UIButton) {
-        
+        print(currentSelectButton)
         drinkButton.alpha = 0.2
         jobButton.alpha = 0.2
         eatButton.alpha = 0.2
