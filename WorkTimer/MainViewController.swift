4ff59f6c30fa1ef0c43fbdc778fc65142fb4ddaa
Grzegorz Kulesza <gkulesza@9bits.pl>
2019-04-09 11:23:30 +0200

    add first project look

---
+++ WorkTimer/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainViewController.swift
+//  WorkTimer
+//
+//  Created by Grzesiek Kulesza on 09/04/2019.
+//  Copyright © 2019 Grzesiek Kulesza. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func summaryButtonPressed(_ sender: Any) {
+        
+       presentDetail(SummaryViewController.init())
+    }
+    
+}
+
+extension UIViewController {
+    
+    func presentDetail(_ viewControllerToPresent: UIViewController) {
+        let transition = CATransition()
+        transition.duration = 0.25
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromRight
+        self.view.window!.layer.add(transition, forKey: kCATransition)
+        
+        present(viewControllerToPresent, animated: false)
+    }
+    
+    func dismissDetail() {
+        let transition = CATransition()
+        transition.duration = 0.25
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromLeft
+        self.view.window!.layer.add(transition, forKey: kCATransition)
+        
+        dismiss(animated: false)
+    }
+}
---
+++ WorkTimer/SummaryViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SummaryViewController.swift
+//  WorkTimer
+//
+//  Created by Grzesiek Kulesza on 09/04/2019.
+//  Copyright © 2019 Grzesiek Kulesza. All rights reserved.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func BackButtonAction(_ sender: Any) {
+        dismissDetail()
+        //dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
